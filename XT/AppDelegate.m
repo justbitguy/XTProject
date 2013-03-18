45311fa49ccbcceaa779ffd7d5d2055008e5cea0
justbitguy <justbitguy@gmail.com>
2013-03-18 23:04:55 +0800

    ios test
    ios test porject

--- XT/AppDelegate.m
+++ XT/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "XTViewController.h"
 
 @implementation AppDelegate
 
@@ -14,6 +15,9 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    XTViewController* viewController = [[XTViewController alloc] init];
+    self.window.rootViewController = viewController;
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
